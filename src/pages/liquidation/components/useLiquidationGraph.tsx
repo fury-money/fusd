05f8292faf955dc4b1e892d58ce9418ff2032deb
CavernPerson <caverprotocol@gmail.com>
2022-12-30 11:46:54 +0100

    Corrected luna price in liquidation page'

--- src/pages/liquidation/components/useLiquidationGraph.tsx
+++ src/pages/liquidation/components/useLiquidationGraph.tsx
@@ -121,7 +121,7 @@ export const useMyLiquidationStats = () => {
         {
           title: 'LUNA Price',
           value: lunaPrice ?? 0,
-          format_func: (v: any) => formatUST(demicrofy(v.toString() as u<UST>)),
+          format_func: (v: any) => v.toString() as UST,
         },
       ],
     };
